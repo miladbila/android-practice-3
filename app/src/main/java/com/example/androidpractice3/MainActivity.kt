b0d4a38ad6cfa1a29c504c0eca9ae8f72cc66590
Milad <milad.tavakoli81@gmail.com>
2024-05-20 10:12:47 +0330

    start foreground

--- app/src/main/java/com/example/androidpractice3/MainActivity.kt
+++ app/src/main/java/com/example/androidpractice3/MainActivity.kt
@@ -1,47 +1,59 @@
 package com.example.androidpractice3
 
+import android.Manifest
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Button
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
 import com.example.androidpractice3.ui.theme.AndroidPractice3Theme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.POST_NOTIFICATIONS),
+                0
+            )
+        }
+
         setContent {
             AndroidPractice3Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally,
+                ) {
+                    Button(onClick = {
+                        Intent(applicationContext, ForegroundService::class.java).also {
+                            it.action = ForegroundService.Actions.START.toString()
+                            startService(it)
+                        }
+                    }) {
+                        Text(text="Star Service")
+                    }
+
+                    Button(onClick = {
+                        Intent(applicationContext, ForegroundService::class.java).also {
+                            it.action = ForegroundService.Actions.STOP.toString()
+                            startService(it)
+                        }
+                    }) {
+                        Text(text="Stop Service")
+                    }
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    AndroidPractice3Theme {
-        Greeting("Android")
     }
 }
